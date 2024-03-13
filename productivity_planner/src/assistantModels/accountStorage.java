ed50af1b82e14f4e6bda243373c68e18c08b73c6
Kevin Lindemann <klindemann@ycp.edu>
2024-03-13 10:34:54 -0400

    Main Upload
    Basic JSP for login working. Web XML needs to be adjusted.

---
+++ productivity_planner/src/assistantModels/accountStorage.java
@@ -0,0 +1,19 @@
+package assistantModels;
+import java.util.ArrayList;
+
+public class accountStorage{
+    private static ArrayList<actorAccount> accountStorage = new ArrayList<>();
+    
+    public void createAccount(String firstName, String lastName, String email, String password){
+    	actorAccount newAccount = new actorAccount();
+    	newAccount.setEmail(email);
+    	newAccount.setFirstName(firstName);
+    	newAccount.setLastName(lastName);
+    	newAccount.setPassword(password);
+    	accountStorage.add(newAccount);
+    }
+    
+    public static void deleteAccount(actorAccount account) {
+    	accountStorage.remove(account);
+    }
+}
