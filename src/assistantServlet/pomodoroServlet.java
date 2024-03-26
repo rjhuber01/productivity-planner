918680f528f931591bad12be79fb234a9301d113
rjhuber01 <rhuber1@ycp.edu>
2024-03-26 18:51:49 -0400

    Update pomodoroServlet.java

--- src/assistantServlet/pomodoroServlet.java
+++ src/assistantServlet/pomodoroServlet.java
@@ -1,11 +1,15 @@
 package assistantServlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import assistantControllers.Assistant;
+import assistantModels.Task;
 
 public class pomodoroServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -15,7 +19,13 @@ public class pomodoroServlet extends HttpServlet {
 			throws ServletException, IOException {
 
 		System.out.println("Pomodoro Servlet: doGet");	
-		
+		HttpSession session = req.getSession(true);
+		Assistant taskController = (Assistant) session.getAttribute("taskController");
+		if(taskController == null) {
+			taskController = new Assistant();
+			session.setAttribute("taskController", taskController);
+		}
+
 		// call JSP to generate empty form
 		req.getRequestDispatcher("/_view/pomoTimer.jsp").forward(req, resp);
 	}
@@ -39,9 +49,8 @@ public class pomodoroServlet extends HttpServlet {
 		// add result objects as attributes
 		// this adds the errorMessage text and the result to the response
 		//req.setAttribute("errorMessage", errorMessage);
-		
 		// Forward to view to render the result HTML document
-		req.getRequestDispatcher("/_view/login.jsp").forward(req, resp);
+		req.getRequestDispatcher("/_view/pomoTimer.jsp").forward(req, resp);
 	}
 
 	// gets double from the request with attribute named s
